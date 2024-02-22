c499a18380f5e3066e79a4ffcf54796c02fe264c
heitzjulien <heitzjulien@pm.me>
2024-02-22 19:04:21 +0100

    feat(reflex.js): add createReflexButton

--- scripts/reflex.js
+++ scripts/reflex.js
@@ -1,1 +1,20 @@
-console.log("reflex");
+const reflexDiv = document.querySelector(".reflex");
+
+function createReflexButton() {
+    const color = "#" + (Math.floor(Math.random() * 0xFFFFFF) + 0x1000000).toString(16).substring(1);
+    let size = 10 + Math.floor(Math.random() * 40);
+    let top = Math.floor( Math.random() * 700 );
+    let left = Math.floor( Math.random() * 1300 );
+
+    reflexDiv.innerHTML = `<button class="btn-reflex" style="all: unset; cursor: pointer; position: absolute; top: ${top}px; left: ${left}px;"><div style="border-radius: 1000px; background: ${color}; width: ${size}px; height: ${size}px;"></div></button>`
+    const btn = document.querySelector(".btn-reflex");
+
+    if (btn) {
+        btn.addEventListener("click", () => {
+            btn.remove();
+            createButton();
+        })
+    }
+}
+
+createReflexButton();
