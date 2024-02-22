8846cdcb8aa46c9c1820147c8ad2cb7f85cdaad7
heitzjulien <heitzjulien@pm.me>
2024-02-22 15:33:43 +0100

    feat(alert): add alert script

--- scripts/alert.js
+++ scripts/alert.js
@@ -1,1 +1,19 @@
-console.log("alert");
+const btn = document.querySelector(".btn-submit")
+
+const alert_array = ["Don't eat that!"]
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let total_alert = getRandomIntInclusive(1, 10);
+
+btn.addEventListener("click", () => {
+    for(let i = 0; i < total_alert; i++) {
+        let random_alert = getRandomIntInclusive(1, 10);
+        total_alert = total_alert - 1;
+        alert(alert_array[random_alert]);
+    }
+})
