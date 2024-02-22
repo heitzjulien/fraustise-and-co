937aebc369a2cfbcc5a88edde9af8589d210fe95
Nyoote <faustinecharrier@gmail.com>
2024-02-22 19:26:34 +0100

    feat: add timer

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,1 +1,21 @@
-console.log("timer");
+document.addEventListener('DOMContentLoaded', function () {
+
+    function updateTimerDisplay(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        const timerDisplay = `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+        document.getElementById('timer').textContent = timerDisplay;
+    }
+
+    let timeInSeconds = 2 * 60;
+
+    function updateTimer() {
+        if (timeInSeconds > 0) {
+            timeInSeconds--;
+            updateTimerDisplay(timeInSeconds);
+        } else {
+            console.log("Timer finished!");
+        }
+    }
+    const timerInterval = setInterval(updateTimer, 1000);
+});
