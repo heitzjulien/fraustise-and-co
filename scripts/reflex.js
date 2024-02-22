36105286eee32434a24a29e85ed8647413fb23c7
heitzjulien <heitzjulien@pm.me>
2024-02-22 21:13:33 +0100

    feat(reflex.js): add setTimeout and setInterval

--- scripts/reflex.js
+++ scripts/reflex.js
@@ -1,10 +1,11 @@
 const reflexDiv = document.querySelector(".reflex");
-
+let missedClicks = 0
+let interval;
 function createReflexButton() {
     const color = "#" + (Math.floor(Math.random() * 0xFFFFFF) + 0x1000000).toString(16).substring(1);
     let size = 10 + Math.floor(Math.random() * 40);
-    let top = Math.floor( Math.random() * 700 );
-    let left = Math.floor( Math.random() * 1300 );
+    let top = Math.floor( Math.random() * window.innerHeight );
+    let left = Math.floor( Math.random() * window.innerWidth );
 
     reflexDiv.innerHTML = `<button class="btn-reflex" style="all: unset; cursor: pointer; position: absolute; top: ${top}px; left: ${left}px;"><div style="border-radius: 1000px; background: ${color}; width: ${size}px; height: ${size}px;"></div></button>`
     const btn = document.querySelector(".btn-reflex");
@@ -12,9 +13,20 @@ function createReflexButton() {
     if (btn) {
         btn.addEventListener("click", () => {
             btn.remove();
-            createReflexButton();
+            missedClicks = 0;
         })
+
+        setTimeout(() => {
+            if (btn.parentNode) {
+                btn.remove();
+                missedClicks++;
+                if (missedClicks === 3) {
+                    console.log("The End")
+                    clearInterval(interval)
+                }
+            }
+        }, 5000);
     }
 }
-
 createReflexButton();
+interval = setInterval(createReflexButton, 15000);
