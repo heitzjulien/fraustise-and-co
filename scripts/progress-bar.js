adebfe0f4a524330e95a00cdccf2fde5566287ec
Nyoote <faustinecharrier@gmail.com>
2024-02-23 10:21:08 +0100

    fix : add random color progress bar

--- scripts/progress-bar.js
+++ scripts/progress-bar.js
@@ -3,9 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
     let progress = 0;
 
     function updateProgressBar() {
+        const color = "#" + (Math.floor(Math.random() * 0xFFFFFF) + 0x100000).toString(16).substring(1);
         progress += 0.01;
         let incoherentProgress = (Math.sin(progress) + 1) / 2 * 100;
         progressBar.style.width = incoherentProgress + '%';
+        progressBar.style.background = color
     }
 
     setInterval(updateProgressBar, 25);
